4c78bff2bdfda8b47b230c4e385c4c13f77b1553
Espen Kåsa Notodden <enotodden@gmail.com>
2014-08-26 18:07:20 +0200

    added support for os-release

--- goosinfo_linux.go
+++ goosinfo_linux.go
@@ -1,9 +1,20 @@
 package goosinfo
 
 import (
+    "io/ioutil"
+    "strings"
+    "os"
     "fmt"
     "syscall"
 )
+
+type OSRelease struct {
+    Name string
+    PrettyName string
+    Version string
+    VersionID string
+    ID string
+}
 
 func uname_chars_to_string(ca [65]int8) string {
     s := make([]byte, len(ca))
@@ -17,13 +28,61 @@ func uname_chars_to_string(ca [65]int8) string {
     return string(s[0:lens])
 }
 
-func GetOSVersion() (string, error) {
-    //FIXME: Read /etc/os-release or similar and then fall back on kernel
-    //       version
+func parse_os_release (contents string) (*OSRelease, error) {
+    osr := OSRelease{}
+    contents = strings.TrimSpace(contents)
+    lines := strings.Split(contents, "\n")
+    for _, line := range(lines) {
+        spl := strings.Split(line, "=")
+        k := strings.ToUpper(spl[0])
+        v := strings.Trim(spl[1], "\"")
+        if k == "NAME" {
+            osr.Name = v
+        } else if k == "PRETTY_NAME" {
+            osr.PrettyName = v
+        } else if k == "VERSION" {
+            osr.Version = v
+        } else if k == "VERSION_ID" {
+            osr.VersionID = v
+        } else if k == "ID" {
+            osr.ID = v
+        }
+    }
+    return &osr, nil
+}
+
+func read_os_release () (*OSRelease, error) {
+    file, err := os.Open("/etc/os-release")
+    if err != nil {
+        return nil, err
+    }
+    c, err := ioutil.ReadAll(file)
+    if err != nil {
+        return nil, err
+    }
+    file.Close()
+    return parse_os_release(string(c))
+}
+
+func get_kernel_version_uname() (string, error) {
     uts := syscall.Utsname{}
     err := syscall.Uname(&uts)
     if err != nil {
         return "", fmt.Errorf("Could not get Linux version")
     }
+    y, _ := read_os_release()
+    fmt.Println(y)
     return uname_chars_to_string(uts.Release), nil
+}
+
+func GetOSVersion() (string, error) {
+    osr, err := read_os_release()
+    if err != nil {
+        kver, err := get_kernel_version_uname()
+        if err != nil {
+            return "", err
+        }
+        return kver, nil
+    }
+    return osr.Version, nil
 }
